43391c73fe1866424e010e328d19dc561a93b078
Spyder #DerpGang <spyderzzz.cbc@gmail.com>
2019-09-26 21:11:02 +0530

    Update hash.py

--- userbot/modules/hash.py
+++ userbot/modules/hash.py
@@ -53,7 +53,7 @@ async def endecrypt(query):
         lething = str(
             pybase64.b64encode(bytes(query.pattern_match.group(2),
                                      "utf-8")))[2:]
-        await query.reply("Encoded: `" + lething[:-1] + "`")
+        await query.reply("Shhh! It's Encoded: `" + lething[:-1] + "`")
     else:
         lething = str(
             pybase64.b64decode(bytes(query.pattern_match.group(2), "utf-8"),
